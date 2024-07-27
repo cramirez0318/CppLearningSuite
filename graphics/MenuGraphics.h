623776895ffa086554ba9237fba2356a2e29723e
ChrisUE5.2 <125326286+cramirez0318@users.noreply.github.com>
2024-07-27 00:51:45 -0700

    a little rework.

--- graphics/MenuGraphics.h
+++ graphics/MenuGraphics.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include "../Menu.h"
 
 bool initSDL();
 void closeSDL();
 void renderText(SDL_Renderer* renderer, const std::string& text, int x, int y, TTF_Font* font, SDL_Color color);
 void renderMainMenu(SDL_Renderer* renderer, const std::vector<std::pair<int, std::string>>& menuOrder, const std::unordered_map<std::string, std::string>& menuNames);
 void handleMenuEvent(SDL_Event& e, std::unordered_map<std::string, MenuFunction>& menuOptions);
+
